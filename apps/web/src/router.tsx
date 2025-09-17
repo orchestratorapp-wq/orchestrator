d0f2b46212aca60aed223957c5e68f8ba61511d1
luka dodelia <luka@lnh.ge>
2025-09-17 09:05:33 +0400

    load from server

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,33 +1,14 @@
-import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
-import { ConvexReactClient } from "convex/react";
 import Loader from "./components/loader";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
 export function createRouter() {
-	const CONVEX_URL =
-		(globalThis as unknown as Record<string, string>)?.VITE_CONVEX_URL || // CF Worker binding (if set as global)
-		(typeof import.meta !== "undefined" && import.meta.env.VITE_CONVEX_URL) || // Vite/browser
-		process.env.VITE_CONVEX_URL ||
-		"https://keen-camel-44.convex.cloud"; // Node.js fallback (dev or edge case)
-
-	if (!CONVEX_URL) {
-		console.error("missing envar VITE_CONVEX_URL");
-	}
-	const convex = new ConvexReactClient(CONVEX_URL, {
-		unsavedChangesWarning: true,
-	});
-
 	const router = createTanStackRouter({
 		routeTree,
 		defaultPreload: "intent",
 		defaultPendingComponent: () => <Loader />,
 		defaultNotFoundComponent: () => <div>Not Found</div>,
-		context: { convexClient: convex },
-		Wrap: ({ children }) => (
-			<ConvexAuthProvider client={convex}>{children}</ConvexAuthProvider>
-		),
 	});
 	return router;
 }
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,18 +1,15 @@
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import {
 	createRootRouteWithContext,
 	HeadContent,
 	Outlet,
 	Scripts,
 } from "@tanstack/react-router";
-import type { ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { Toaster } from "@/components/ui/sonner";
 import appCss from "../index.css?url";
 
-export interface RouterAppContext {
-	convexClient: ConvexReactClient;
-}
-
-export const Route = createRootRouteWithContext<RouterAppContext>()({
+export const Route = createRootRouteWithContext()({
 	head: () => ({
 		meta: [
 			{
@@ -33,18 +30,33 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 			},
 		],
 	}),
+	loader: async () => {
+		const CONVEX_URL =
+			(globalThis as unknown as Record<string, string>)?.VITE_CONVEX_URL || // CF Worker binding (if set as global)
+			(typeof import.meta !== "undefined" && import.meta.env.VITE_CONVEX_URL) || // Vite/browser
+			process.env.VITE_CONVEX_URL;
 
+		return { CONVEX_URL };
+	},
 	component: RootDocument,
 });
 
 function RootDocument() {
+	const loaderData = Route.useLoaderData() || [];
+
+	const convex = new ConvexReactClient(loaderData.CONVEX_URL, {
+		unsavedChangesWarning: true,
+	});
+
 	return (
 		<html lang="en" className="dark h-full bg-white dark:bg-gray-900">
 			<head>
 				<HeadContent />
 			</head>
 			<body className="h-full">
-				<Outlet />
+				<ConvexAuthProvider client={convex}>
+					<Outlet />
+				</ConvexAuthProvider>
 				<Toaster richColors />
 				{/*<TanStackRouterDevtools position="bottom-left" />*/}
 				<Scripts />
